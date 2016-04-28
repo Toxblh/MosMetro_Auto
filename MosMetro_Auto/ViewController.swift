1115d0acbaa8468e94085604e72c22e3c3cae740
Anton Palgunov <toxblh@gmail.com>
2016-04-29 00:49:25 +0300

    work in dummy.. test in metro

--- MosMetro_Auto/ViewController.swift
+++ MosMetro_Auto/ViewController.swift
@@ -5,7 +5,6 @@
 //  Created by Anton Palgunov on 25.04.16.
 //  Copyright © 2016 toxblh. All rights reserved.
 //
-
 import UIKit
 import SystemConfiguration.CaptiveNetwork
 
@@ -22,11 +21,11 @@ class ViewController: UIViewController {
 
     // Button
     @IBAction func GetSSID(sender: AnyObject) {
-        let NameWifi = getSSID()
-        WifiName.text = NameWifi
-        if (NameWifi ==  SSID_Metro) {
-            DebugLog("Вы в метро!")
-        }
+//        let NameWifi = getSSID()
+//        WifiName.text = NameWifi
+//        if (NameWifi ==  SSID_Metro) {
+//            DebugLog("Вы в метро!")
+//        }
 
     }
 
@@ -45,12 +44,15 @@ class ViewController: UIViewController {
     }
 
 
-    func DebugLog(newLine: NSString) {
-        let text = "\n\(newLine as String)"
-        DebugText.text = DebugText.text.stringByAppendingString(text)
+    func DebugLog(newLine: AnyObject) {
+        print(newLine)
+        let text = "\n\(String(newLine))"
+        dispatch_async(dispatch_get_main_queue(), {
+            self.DebugText.text = self.DebugText.text.stringByAppendingString(text)
+        })
     }
 
-    func httpRequest(completion: (html: NSString?, headers: NSObject? ,error: NSError?)->(), url: String) {
+    func httpRequest(url: String, completion: (html: NSString?, headers: NSObject?, code: Int?,error: String?)->()) {
         let session = NSURLSession.sharedSession()
         let getUrl = NSURL(string: url)
 
@@ -58,8 +60,7 @@ class ViewController: UIViewController {
             (data, response, error) -> Void in
 
             if error != nil {
-                print(error?.localizedDescription)
-                completion(html: nil, headers: nil, error: error)
+                completion(html: nil, headers: nil, code: nil, error: (error!.localizedDescription) as String)
             } else {
                 let result = NSString(data: data!, encoding:
                     NSUTF8StringEncoding)!
@@ -67,16 +68,11 @@ class ViewController: UIViewController {
                 if let httpUrlResponse = response as? NSHTTPURLResponse
                 {
                     if error != nil {
-                        print("Error Occurred: \(error!.localizedDescription)")
+                        self.DebugLog("Error Occurred: \(error!.localizedDescription)")
                     } else {
                         let headers = httpUrlResponse.allHeaderFields
-                        print(headers.count)
-                        print(headers["Server"])
-                        print(headers.values)
-                        print("\(headers)")
-                        print(httpUrlResponse.statusCode)
-                        
-                        completion(html: result, headers: headers, error: nil)
+                        let code = httpUrlResponse.statusCode
+                        completion(html: result, headers: headers, code: code, error: nil)
                     }
                 }
             }
@@ -84,20 +80,45 @@ class ViewController: UIViewController {
         
         task.resume()
     }
-
+    
+    func DLog(html: NSString?, head: NSObject?, code: Int?, error: String?) {
+        if error == nil {
+            DebugLog(code!);
+            DebugLog(head!);
+            DebugLog(html!);
+        } else {
+            DebugLog(error!);
+        }
+        
+    }
+    
+    func matchesForRegexInText(regex: String!, text: String!) -> [String] {
+        
+        do {
+            let regex = try NSRegularExpression(pattern: regex, options: [])
+            let nsString = text as NSString
+            let results = regex.matchesInString(text,
+                                                options: [], range: NSMakeRange(0, nsString.length))
+            return results.map { nsString.substringWithRange($0.range)}
+        } catch let error as NSError {
+            print("invalid regex: \(error.localizedDescription)")
+            return []
+        }
+    }
 
     func connectInternet() -> Bool {
+        var RedirectURL = ""
         print("Connect")
 
-        DebugLog("> Подключение к сети " + getSSID());
+        //DebugLog("> Подключение к сети " + getSSID());
         DebugLog(">> Проверка доступа в интернет");
-        var connected = checkInternet();
+        let connected = checkInternet();
         
         if (connected) {
-            DebugLog("<< Уже подключено");
+            DebugLog("<< Уже есть");
+            //return true; //Раз есть то смысла нет
         } else  {
-            DebugLog("<< Ошибка: Сеть недоступна или не отвечает");
-            return false;
+            DebugLog("<< Интернета нет");
         }
 
 
@@ -107,65 +128,86 @@ class ViewController: UIViewController {
             let contents = try NSString(contentsOfURL: url, usedEncoding: nil)
             if (contents != "") {
                 DebugLog(contents)
+                
+                let findRedirect = matchesForRegexInText("URL=([?=&\\da-z\\.-:\\.\\/\\w \\.-]*)", text: contents as String)
+                DebugLog(findRedirect)
+                
+                    let rawUrlRedirect = String(findRedirect)
+                    if rawUrlRedirect != "[]" {
+                        let rangeURL = Range(rawUrlRedirect.startIndex.advancedBy(6)..<rawUrlRedirect.endIndex.advancedBy(-2))
+                        RedirectURL = rawUrlRedirect[rangeURL]
+                        DebugLog(RedirectURL)
+                    } else {
+                        DebugLog("<<< Ошибка: перенаправление не найдено")
+                        DebugLog("^<<Нет ссылки")
+                        return false
+                }
+            } else {
+                DebugLog("<<< Ошибка: перенаправление не найдено")
+                DebugLog("^<<Нет контента")
+                //return false
                 }
             } catch {
-                
+                DebugLog("[Error:1] Эксепшон!!")
+                //return false
             }
         }
         
-//        httpRequest({(html: NSString?, headers: NSObject?, error: NSError?) -> Void in
-//            self.DebugLog(html!)
-//        }, url: "https://ya.ru")
-        
-/*
-        if (link = client.parseMetaRedirect()) {
-            DebugLog(link);
-        } else {
-            DebugLog("<<< Ошибка: перенаправление не найдено");
-            return false;
-        }
 
         DebugLog(">>> Получение страницы авторизации");
-        if (client.get(link)) {
-            DebugLog(client.getPageContent().outerHtml());
-        } else {
-            DebugLog("<<< Ошибка: страница авторизации не получена");
-            return false;
-        }
-
-        if (Elements forms = client.getPageContent().getElementsByTag("form")) {
-            if (forms.size() > 1 && forms.last().attr("id").equals("sms-form")) {
-                DebugLog("<<< Ошибка: устройство не зарегистрировано в сети");
-
-                DebugLog("\nПожалуйста, зайдите на сайт http://wi-fi.ru и пройдите регистрацию, " +
-                    "введя свой номер телефона в появившуюся форму для получения СМС с дальнейшими инструкциями. " +
-                    "Это необходимо сделать только один раз, после чего приложение начнет нормально работать.");
-
-                DebugLog("\nПримечание: Разработчик этого приложения не имеет никакого отношения к регистрации. " +
-                    "Регистрацией, как и самой сетью, занимается компания МаксимаТелеком (http://maximatelecom.ru).");
-                return false;
-            }
-            fields = Client.parseForm(forms.first());
-        } else {
-            DebugLog("<<< Ошибка: форма авторизации не найдена");
-            return false;
-        }
-
+        
+        let tutorialsURL = NSURL(string: RedirectURL)
+        let htmlData: NSData = NSData(contentsOfURL: tutorialsURL!)!
+        let input = NSString(data: htmlData, encoding: NSUTF8StringEncoding)
+        DebugLog(input!)
+        
+        let findCsrfSign = matchesForRegexInText("<input type=\"hidden\" name=\"csrf\\.sign\" value=\"[0-9a-z]*\"\\/>", text: input as! String)
+        let rawCsrfSign = String(findCsrfSign)
+        let rangeCsrfSign = rawCsrfSign.startIndex.advancedBy(52)..<rawCsrfSign.endIndex.advancedBy(-6)
+        let csrfSign = rawCsrfSign[rangeCsrfSign]
+        DebugLog(csrfSign)
+        
+        let findCsrfTs = matchesForRegexInText("<input type=\"hidden\" name=\"csrf\\.ts\" value=\"[0-9a-z]*\"\\/>", text: input as! String)
+        let rawCsrfTs = String(findCsrfTs)
+        let rangeCsrfTs = rawCsrfTs.startIndex.advancedBy(50)..<rawCsrfTs.endIndex.advancedBy(-6)
+        let csrfTs = rawCsrfTs[rangeCsrfTs]
+        DebugLog(csrfTs)
+        
+        
         DebugLog(">>> Отправка формы авторизации");
-        if (client.post(link, fields)) {
-            DebugLog(client.getPageContent().outerHtml());
-        } else {
-            DebugLog("<<< Ошибка: сервер не ответил или вернул ошибку");
-            return false;
+        
+        let url:NSURL = NSURL(string: RedirectURL)!
+        let session = NSURLSession.sharedSession()
+        
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "POST"
+        request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringCacheData
+        
+        let paramString = "promogoto=&IDButton=Confirm&csrf.sign="+csrfSign+"&csrf.ts="+csrfTs
+        request.HTTPBody = paramString.dataUsingEncoding(NSUTF8StringEncoding)
+        DebugLog(request)
+        let task = session.dataTaskWithRequest(request) {
+            (
+            let data, let response, let error) in
+            
+            guard let _:NSData = data, let _:NSURLResponse = response  where error == nil else {
+                self.DebugLog("<<< Ошибка: сервер не ответил или вернул ошибку");
+                return
+            }
+            
+            let dataString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            self.DebugLog(dataString!)
+            
+            self.DebugLog(">> Проверка доступа в интернет");
+            if (self.checkInternet()) {
+                self.DebugLog("<< Соединение успешно установлено :3");
+            } else {
+                self.DebugLog("<< Ошибка: доступ в интернет отсутствует");
+            }
         }
-
-        DebugLog(">> Проверка доступа в интернет");
-        if (checkInternet()) {
-            DebugLog("<< Соединение успешно установлено :3");
-        } else {
-            DebugLog("<< Ошибка: доступ в интернет отсутствует");
-            return false;
-        }*/
+        
+        task.resume()
+        
         return false;
     }
 
@@ -199,21 +241,44 @@ class ViewController: UIViewController {
     }
 
 
-    func getSSID() ->  String {
-        var currentSSID = ""
-        if let interfaces:CFArray! = CNCopySupportedInterfaces() {
-            for i in 0..<CFArrayGetCount(interfaces){
-                let interfaceName: UnsafePointer<Void> = CFArrayGetValueAtIndex(interfaces, i)
-                let rec = unsafeBitCast(interfaceName, AnyObject.self)
-                let unsafeInterfaceData = CNCopyCurrentNetworkInfo("\(rec)")
-                if unsafeInterfaceData != nil {
-                    let interfaceData = unsafeInterfaceData! as Dictionary!
-                    currentSSID = interfaceData["SSID"] as! String
-                }
-            }
-        }
-        return currentSSID
-    }
+//    func getSSID() ->  String {
+//        var currentSSID = ""
+//        if let interfaces:CFArray! = CNCopySupportedInterfaces() {
+//            for i in 0..<CFArrayGetCount(interfaces){
+//                let interfaceName: UnsafePointer<Void> = CFArrayGetValueAtIndex(interfaces, i)
+//                let rec = unsafeBitCast(interfaceName, AnyObject.self)
+//                let unsafeInterfaceData = CNCopyCurrentNetworkInfo("\(rec)")
+//                if unsafeInterfaceData != nil {
+//                    let interfaceData = unsafeInterfaceData! as Dictionary!
+//                    currentSSID = interfaceData["SSID"] as! String
+//                }
+//            }
+//        }
+//        return currentSSID
+//        if let list = cmd.networkList where cmd.commandType == .FilterScanList {
+//            var networks = [NEHotspotNetwork]()
+//            for network in list {
+//                if network.SSID.hasPrefix("BTVNET") {
+//                    network.setPassword("12345678")
+//                    network.setConfidence(.High)
+//                    networks.append(network)
+//                }
+//            }
+//            let response = cmd.createResponse(.Success)
+//            response.setNetworkList(networks)  
+//            response.deliver()  
+//        }
+//        
+//        
+//        hotspotNetwork = NEHotspotHelper supportedNetworkInterfaces
+//            NSString ssid = hotspotNetwork.SSID;
+//            NSString bssid = hotspotNetwork.BSSID;
+//            BOOL secure = hotspotNetwork.secure;
+//            BOOL autoJoined = hotspotNetwork.autoJoined;
+//            double signalStrength = hotspotNetwork.signalStrength;
+//
+//        return ""
+//    }
 
     func checkInternet() -> Bool {
 
